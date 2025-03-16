4c829780b334f046f82d492fda4167f04dfb82a3
FreddyD13 <freddydormevil@hotmail.com>
2025-03-16 02:47:10 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fe4fc4f0-76d1-40e8-adbf-293e405c2b7c';
+export const MODAL_CLOSE_SEC = 2500;
